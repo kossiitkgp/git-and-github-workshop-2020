5eff5ae8bfdbc14d73a27ee33db5dc8a3e775bae
Bineet Singh Chadha <bineetchadha@gmail.com>
2020-10-09 18:47:46 +0530

    subtract_two_numbers function added

--- code_samples/sample.py
+++ code_samples/sample.py
@@ -5,7 +5,21 @@ def add_two_numbers(a,b):
     Args:
         a,b : two real numbers
     '''
-    sum = a - b
+    sum = a + b
     print(sum)
 
 add_two_numbers(5,2) # expected result 7
+
+def subtract_two_numbers(a,b):
+    '''
+    The function takes in 2 numbers and prints their difference
+    
+    Args:
+        a,b : two real numbers
+    '''
+    
+    sub = a -b 
+    print(sub)
+    
+subtract_two_numbers(7,3) # expected result 4
+                         
