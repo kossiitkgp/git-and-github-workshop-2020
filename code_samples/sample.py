2767dfd23c8bfdac4400bde7947d3a7d51311063
OtchereDev <59093261+OtchereDev@users.noreply.github.com>
2020-10-09 14:05:45 +0000

    Add subtraction function

--- code_samples/sample.py
+++ code_samples/sample.py
@@ -9,3 +9,23 @@ def add_two_numbers(a,b):
     print(sum)
 
 add_two_numbers(5,2) # expected result 7
+
+def subtract_two_numbers(a,b):
+
+    '''
+
+    The function takes in 2 numbers and prints their difference
+
+    
+
+    Args:
+
+        a,b : two real numbers
+
+    '''
+
+    diffence = a - b
+
+    print(difference)
+
+subtract_two_numbers(5,2) # expected result 3
