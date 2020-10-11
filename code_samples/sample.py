c72fdbfd98d462ce77fb081152566997c0f21a82
Mayank Gupta <mayank.gupta61@ad.infosys.com>
2020-10-11 18:18:38 +0530

    Added the subtract functoin in sample.py file

--- code_samples/sample.py
+++ code_samples/sample.py
@@ -1,4 +1,4 @@
-def add_two_numbers(a,b):
+def add_two_numbers(a, b):
     '''
     The function takes in 2 numbers and prints their sum
     
@@ -8,4 +8,20 @@ def add_two_numbers(a,b):
     sum = a + b
     print(sum)
 
-add_two_numbers(5,2) # expected result 7
+
+def subtract_two_numbers(a, b):
+    """
+    The function takes in 2 numbers and prints their difference
+
+    Args:
+        a,b : two real numbers
+    """
+
+    diff = a - b
+    if b > a:
+        diff = b - a
+    print(diff)
+
+
+add_two_numbers(5, 2)  # expected result 7
+subtract_two_numbers(5, 2)  # expected result 3
