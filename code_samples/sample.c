66a3109820096e701c1002bc931f2bba2e3031e3
Deveshgsk <56759703+Deveshgsk@users.noreply.github.com>
2020-10-12 09:25:22 +0530

    added feature Sub 2 num in sample.c

--- code_samples/sample.c
+++ code_samples/sample.c
@@ -6,7 +6,13 @@ int add_two_numbers(a,b) {
     printf("%d", sum);
 }
 
+int sub_two_numbers(a,b) {
+    int sum;
+    sum = a - b;
+    printf("%d", sum);
+}
 
 int main() {
     add_two_numbers(5,2); // expected result 7
+    sub_two_numbers(5,2); // expected result 3
 }
